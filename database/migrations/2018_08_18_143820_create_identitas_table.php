3e1593c9092d550a03dda4ac8763aaec3d09ed0e
Samodra <samodra30@gmail.com>
2018-08-18 21:51:42 +0700

    Revisi 2

--- database/migrations/2018_08_18_143820_create_identitas_table.php
+++ database/migrations/2018_08_18_143820_create_identitas_table.php
@@ -14,7 +14,15 @@ class CreateIdentitasTable extends Migration
     public function up()
     {
         Schema::create('identitas', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id_identitas');
+            $table->string('nama_instansi');
+            $table->text('alamat');
+            $table->string('kota');
+            $table->string('telepon');
+            $table->string('website');
+            $table->string('keuangan');
+            $table->string('logo');
+            $table->integer('spp_perbulan');
             $table->timestamps();
         });
     }
